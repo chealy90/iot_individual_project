18837a82917e6a1b4372642f70a34ea39e16d1f0
chealy90 <christopherhealy20@gmail.com>
2025-12-22 01:24:39 +0000

    feat: Add Login and Register routes to flask app structure
    Created the routes and functions in app.py for login, register, and logout.

--- server/app.py
+++ server/app.py
@@ -8,9 +8,36 @@ def index():
     return render_template("index.html")
 
 
-@app.route('/sensors')
+@app.route('/sensors', methods=["GET", "POST"])
 def sensors():
-    return render_template("sensors_monitor.html")
+    sensor1 = {
+        "sensor_name": "Fridge Sensor",
+        "sensor_current": 1.4,
+        "sensor_min": 1,
+        "sensor_max": 4
+    }
+
+    sensor2 = {
+        "sensor_name": "Freezer Sensor",
+        "sensor_current": -5,
+        "sensor_min": -10,
+        "sensor_max": -3
+    }
+
+    return render_template("sensors_monitor.html", sensors=[sensor1, sensor2])
+
+
+@app.route('/login')
+def login():
+    pass
+
+@app.route('/logout')
+def logout():
+    pass
+
+@app.route('/register')
+def register():
+    pass
 
 
 
