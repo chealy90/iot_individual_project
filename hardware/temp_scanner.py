d2a15a69708b762f095a191d2e7b1fb8b4dafbb1
chealy90 <christopherhealy20@gmail.com>
2025-12-24 15:57:29 +0000

    feat: Incorporate LED
    Added the LED to the system, for testing purposes LED flashes when temp drops below room temperature.

--- hardware/temp_scanner.py
+++ hardware/temp_scanner.py
@@ -1,16 +1,28 @@
 import adafruit_dht
+import digitalio
 from pubnub_client import publish_msg
 import time
 import datetime
 
 
 #IO Pins
-from board import D2 #GPIO 2
+from board import D2, D3 #GPIO PINS
+
+MIN_TEMP = 20
 
 
+def flash_led(led):
+    for i in range(3):
+        led.value = True
+        time.sleep(0.2)
+        led.value = False
+        time.sleep(0.2)
+        
 
 try:
     dht_device = adafruit_dht.DHT22(D2, use_pulseio=False)
+    led_light = digitalio.DigitalInOut(D3)
+    led_light.direction = digitalio.Direction.OUTPUT
     
 
     while True:
@@ -20,6 +32,10 @@ try:
             msg = f"{curr_time} Temp: {temperature} ° C"
             print(msg)
             publish_msg(msg)
+
+            #LED
+            if temperature < MIN_TEMP:
+                flash_led(led_light)
             time.sleep(3)
         except Exception as e:
             print("Error reading temperature: ", e)
