11b5e6a3c951646407ae5e015177ab0e4a1c8626
chealy90 <christopherhealy20@gmail.com>
2025-12-27 02:28:44 +0000

    feat: Run buzzer and LED simultaneously
    Make the code asynchronous so the alarm and LED will both go off at the same time.

--- hardware/temp_scanner.py
+++ hardware/temp_scanner.py
@@ -1,8 +1,11 @@
+#https://www.geeksforgeeks.org/python/python-async/
+
 import adafruit_dht
 import digitalio
 from pubnub_client import publish_msg
 import time
 import datetime
+import asyncio
 
 
 #IO Pins
@@ -11,47 +14,57 @@ from board import D2, D3, D4 #GPIO PINS
 MIN_TEMP = 20
 
 
-def flash_led(led):
+async def flash_led(led):
     for i in range(3):
         led.value = True
-        time.sleep(0.2)
+        await asyncio.sleep(0.2)
         led.value = False
-        time.sleep(0.2)
+        await asyncio.sleep(0.2)
 
-def buzz_alarm(buzzer):  
+
+
+async def buzz_alarm(buzzer):  
     for i in range(3):
         for pulse in range(60):
             buzzer.value = True
-            time.sleep(0.001)
+            await asyncio.sleep(0.001)
             buzzer.value = False
-            time.sleep(0.01)
-        time.sleep(0.02)
+            await asyncio.sleep(0.01)
+        await asyncio.sleep(0.02)
 
+
+async def activate_temp_alarms(buzzer, led):
+    await asyncio.gather(buzz_alarm(buzzer), flash_led(led))
         
 
-try:
-    dht_device = adafruit_dht.DHT22(D2, use_pulseio=False)
-    led_light = digitalio.DigitalInOut(D3)
-    led_light.direction = digitalio.Direction.OUTPUT
 
-    buzzer = digitalio.DigitalInOut(D3)
-    buzzer.direction = digitalio.Direction.OUTPUT
-    
+async def main():
+    try:
+        dht_device = adafruit_dht.DHT22(D2, use_pulseio=False)
+        led_light = digitalio.DigitalInOut(D3)
+        led_light.direction = digitalio.Direction.OUTPUT
 
-    while True:
-        try:
-            temperature = dht_device.temperature
-            curr_time = datetime.datetime.now()
-            msg = f"{curr_time} Temp: {temperature} ° C"
-            print(msg)
-            publish_msg(msg)
+        buzzer = digitalio.DigitalInOut(D4)
+        buzzer.direction = digitalio.Direction.OUTPUT
+        
 
-            #LED / Buzzer
-            if temperature < MIN_TEMP:
-                buzz_alarm(buzzer)
-            time.sleep(3)
-        except Exception as e:
-            print("Error reading temperature: ", e)
+        while True:
+            try:
+                temperature = dht_device.temperature
+                curr_time = datetime.datetime.now()
+                msg = f"{curr_time} Temp: {temperature} ° C"
+                print(msg)
+                publish_msg(msg)
 
-except:
-    print("Error setting up device")
+                #LED / Buzzer
+                if temperature < MIN_TEMP:
+                    await activate_temp_alarms(buzzer, led_light)
+                await asyncio.sleep(3)
+            except Exception as e:
+                print("Error reading temperature: ", e)
+
+    except:
+        print("Error setting up device")
+
+if __name__ == "__main__":
+    asyncio.run(main())
